2af4bfe8c60a39a041ffdca8950a184a820f801a
Tristan Floyd <tlfloyd2@gmail.com>
2024-04-25 15:46:17 -0500

    posts finished

--- main.js
+++ main.js
@@ -38,11 +38,32 @@ function blank() {
 
 function addGameScreen() {
     var s = [];
-    s.push("<form id=\"myForm\">\n<b class=\"topText\">Add Game:</b>\n<p>\n<label for=\"teamId1\">Team 1 ID:</label>\n<input type=\"text\" id=\"teamId1\" name=\"teamId1\"><br><br>");
-    s.push("\n</p>\n<p>\n<label for=\"teamId2\">Team 2 ID:</label>\n<input type=\"text\" id=\"teamId2\" name=\"teamId2\"><br><br>\n</p>\n<p>\n<label for=\"score1\">Team 1 Score:</label>");
-    s.push("\n<input type=\"text\" id=\"score1\" name=\"score1\"><br><br>\n</p>\n<p>\n<label for=\"score1\">Team 1 Score:</label>\n<input type=\"text\" id=\"score1\" name=\"score1\"><br><br>");
-    s.push("\n</p>\n<p>\n<label for=\"score2\">Team 2 Score:</label>\n<input type=\"text\" id=\"score2\" name=\"score2\"><br><br>\n</p>\n<p>\n<label for=\"date\">Date:</label>\n<input type=\"text\" id=\"date\" name=\"date\"><br><br>");
-    s.push("\n</p>\n<p>\n<button class=\"submitButton\" type=\"button\" onclick=\"submitForm()\">Submit</button>\n</p>\n</form>");
+    s.push("<form id=\"myForm\">")
+    s.push("<b class=\"topText\">Add Game:</b>")
+    s.push("<p>")
+    s.push("<label for=\"teamId1\">Team 1 ID:</label>")
+    s.push("<input type=\"text\" id=\"teamId1\" name=\"teamId1\"><br><br>")
+    s.push("</p>")
+    s.push("<p>")
+    s.push("<label for=\"teamId2\">Team 2 ID:</label>")
+    s.push("<input type=\"text\" id=\"teamId2\" name=\"teamId2\"><br><br>")
+    s.push("</p>")
+    s.push("<p>")
+    s.push("<label for=\"score1\">Team 1 Score:</label>")
+    s.push("<input type=\"text\" id=\"score1\" name=\"score1\"><br><br>")
+    s.push("</p>")
+    s.push("<p>")
+    s.push("<label for=\"score2\">Team 2 Score:</label>")
+    s.push("<input type=\"text\" id=\"score2\" name=\"score2\"><br><br>")
+    s.push("</p>")
+    s.push("<p>")
+    s.push("<label for=\"date\">Date:</label>")
+    s.push("<input type=\"text\" id=\"date\" name=\"date\"><br><br>")
+    s.push("</p>")
+    s.push("<p>")
+    s.push("<button class=\"submitButton\" type=\"button\" onclick=\"submitForm()\">Submit</button>")
+    s.push("</p>")
+    s.push("</form>")
     
     var content = document.getElementById('inputs');
     content.innerHTML = s.join('')
@@ -50,47 +71,65 @@ function addGameScreen() {
 
 function addPlayerScreen() {
     var s = [];
-    s.push("<form id=\"myForm\">\n<b class=\"topText\">Add Game:</b>\n<p>\n<label for=\"teamId1\">Team 1 ID:</label>\n<input type=\"text\" id=\"teamId1\" name=\"teamId1\"><br><br>");
-    s.push("\n</p>\n<p>\n<label for=\"teamId2\">Team 2 ID:</label>\n<input type=\"text\" id=\"teamId2\" name=\"teamId2\"><br><br>\n</p>\n<p>\n<label for=\"score1\">Team 1 Score:</label>");
-    s.push("\n<input type=\"text\" id=\"score1\" name=\"score1\"><br><br>\n</p>\n<p>\n<label for=\"score1\">Team 1 Score:</label>\n<input type=\"text\" id=\"score1\" name=\"score1\"><br><br>");
-    s.push("\n</p>\n<p>\n<label for=\"score2\">Team 2 Score:</label>\n<input type=\"text\" id=\"score2\" name=\"score2\"><br><br>\n</p>\n<p>\n<label for=\"date\">Date:</label>\n<input type=\"text\" id=\"date\" name=\"date\"><br><br>");
-    s.push("\n</p>\n<p>\n<button class=\"submitButton\" type=\"button\" onclick=\"submitForm()\">Submit</button>\n</p>\n</form>");
-    
+    s.push("<form id=\"myForm\">")
+    s.push("<b class=\"topText\">Add Player:</b>")
+    s.push("<p>")
+    s.push("<label for=\"teamID\">Team ID:</label>")
+    s.push("<input type=\"text\" id=\"teamID\" name=\"teamID\"><br><br>")
+    s.push("</p>")
+    s.push("<p>")
+    s.push("<label for=\"name\">Name:</label>")
+    s.push("<input type=\"text\" id=\"name\" name=\"name\"><br><br>")
+    s.push("</p>")
+    s.push("<p>")
+    s.push("<label for=\"position\">Position:</label>")
+    s.push("<input type=\"text\" id=\"position\" name=\"position\"><br><br>")
+    s.push("</p>")
+    s.push("<p>")
+    s.push("<button class=\"submitButton\" type=\"button\" onclick=\"submitForm()\">Submit</button>")
+    s.push("</p>")
+    s.push("</form>")
+
     var content = document.getElementById('inputs');
     content.innerHTML = s.join('')
 }
 
 function viewPlayersTeamScreen() {
     var s = [];
-    s.push("<form id=\"myForm\">\n<b class=\"topText\">Add Game:</b>\n<p>\n<label for=\"teamId1\">Team 1 ID:</label>\n<input type=\"text\" id=\"teamId1\" name=\"teamId1\"><br><br>");
-    s.push("\n</p>\n<p>\n<label for=\"teamId2\">Team 2 ID:</label>\n<input type=\"text\" id=\"teamId2\" name=\"teamId2\"><br><br>\n</p>\n<p>\n<label for=\"score1\">Team 1 Score:</label>");
-    s.push("\n<input type=\"text\" id=\"score1\" name=\"score1\"><br><br>\n</p>\n<p>\n<label for=\"score1\">Team 1 Score:</label>\n<input type=\"text\" id=\"score1\" name=\"score1\"><br><br>");
-    s.push("\n</p>\n<p>\n<label for=\"score2\">Team 2 Score:</label>\n<input type=\"text\" id=\"score2\" name=\"score2\"><br><br>\n</p>\n<p>\n<label for=\"date\">Date:</label>\n<input type=\"text\" id=\"date\" name=\"date\"><br><br>");
-    s.push("\n</p>\n<p>\n<button class=\"submitButton\" type=\"button\" onclick=\"submitForm()\">Submit</button>\n</p>\n</form>");
-    
+    s.push("<form id=\"myForm\">")
+    s.push("<b class=\"topText\">View Players:</b>")
+    s.push("<p>")
+    s.push("<label for=\"teamID\">Team ID:</label>")
+    s.push("<input type=\"text\" id=\"teamID\" name=\"teamID\"><br><br>")
+    s.push("</p>")
+    s.push("<p>")
+    s.push("<button class=\"submitButton\" type=\"button\" onclick=\"submitForm()\">Submit</button>")
+    s.push("</p>")
+    s.push("</form>")
+
     var content = document.getElementById('inputs');
     content.innerHTML = s.join('')
 }
 
 function viewPlayersPositionScreen() {
     var s = [];
-    s.push("<form id=\"myForm\">\n<b class=\"topText\">Add Game:</b>\n<p>\n<label for=\"teamId1\">Team 1 ID:</label>\n<input type=\"text\" id=\"teamId1\" name=\"teamId1\"><br><br>");
-    s.push("\n</p>\n<p>\n<label for=\"teamId2\">Team 2 ID:</label>\n<input type=\"text\" id=\"teamId2\" name=\"teamId2\"><br><br>\n</p>\n<p>\n<label for=\"score1\">Team 1 Score:</label>");
-    s.push("\n<input type=\"text\" id=\"score1\" name=\"score1\"><br><br>\n</p>\n<p>\n<label for=\"score1\">Team 1 Score:</label>\n<input type=\"text\" id=\"score1\" name=\"score1\"><br><br>");
-    s.push("\n</p>\n<p>\n<label for=\"score2\">Team 2 Score:</label>\n<input type=\"text\" id=\"score2\" name=\"score2\"><br><br>\n</p>\n<p>\n<label for=\"date\">Date:</label>\n<input type=\"text\" id=\"date\" name=\"date\"><br><br>");
-    s.push("\n</p>\n<p>\n<button class=\"submitButton\" type=\"button\" onclick=\"submitForm()\">Submit</button>\n</p>\n</form>");
-    
+    s.push("<form id=\"myForm\">")
+    s.push("<b class=\"topText\">View Players:</b>")
+    s.push("<p>")
+    s.push("<label for=\"position\">Position:</label>")
+    s.push("<input type=\"text\" id=\"position\" name=\"position\"><br><br>")
+    s.push("</p>")
+    s.push("<p>")
+    s.push("<button class=\"submitButton\" type=\"button\" onclick=\"submitForm()\">Submit</button>")
+    s.push("</p>")
+    s.push("</form>")
+
     var content = document.getElementById('inputs');
     content.innerHTML = s.join('')
 }
 
 function viewTeamsScreen() {
     var s = [];
-    s.push("<form id=\"myForm\">\n<b class=\"topText\">Add Game:</b>\n<p>\n<label for=\"teamId1\">Team 1 ID:</label>\n<input type=\"text\" id=\"teamId1\" name=\"teamId1\"><br><br>");
-    s.push("\n</p>\n<p>\n<label for=\"teamId2\">Team 2 ID:</label>\n<input type=\"text\" id=\"teamId2\" name=\"teamId2\"><br><br>\n</p>\n<p>\n<label for=\"score1\">Team 1 Score:</label>");
-    s.push("\n<input type=\"text\" id=\"score1\" name=\"score1\"><br><br>\n</p>\n<p>\n<label for=\"score1\">Team 1 Score:</label>\n<input type=\"text\" id=\"score1\" name=\"score1\"><br><br>");
-    s.push("\n</p>\n<p>\n<label for=\"score2\">Team 2 Score:</label>\n<input type=\"text\" id=\"score2\" name=\"score2\"><br><br>\n</p>\n<p>\n<label for=\"date\">Date:</label>\n<input type=\"text\" id=\"date\" name=\"date\"><br><br>");
-    s.push("\n</p>\n<p>\n<button class=\"submitButton\" type=\"button\" onclick=\"submitForm()\">Submit</button>\n</p>\n</form>");
     
     var content = document.getElementById('inputs');
     content.innerHTML = s.join('')
@@ -98,24 +137,34 @@ function viewTeamsScreen() {
 
 function viewGamesByTeamScreen() {
     var s = [];
-    s.push("<form id=\"myForm\">\n<b class=\"topText\">Add Game:</b>\n<p>\n<label for=\"teamId1\">Team 1 ID:</label>\n<input type=\"text\" id=\"teamId1\" name=\"teamId1\"><br><br>");
-    s.push("\n</p>\n<p>\n<label for=\"teamId2\">Team 2 ID:</label>\n<input type=\"text\" id=\"teamId2\" name=\"teamId2\"><br><br>\n</p>\n<p>\n<label for=\"score1\">Team 1 Score:</label>");
-    s.push("\n<input type=\"text\" id=\"score1\" name=\"score1\"><br><br>\n</p>\n<p>\n<label for=\"score1\">Team 1 Score:</label>\n<input type=\"text\" id=\"score1\" name=\"score1\"><br><br>");
-    s.push("\n</p>\n<p>\n<label for=\"score2\">Team 2 Score:</label>\n<input type=\"text\" id=\"score2\" name=\"score2\"><br><br>\n</p>\n<p>\n<label for=\"date\">Date:</label>\n<input type=\"text\" id=\"date\" name=\"date\"><br><br>");
-    s.push("\n</p>\n<p>\n<button class=\"submitButton\" type=\"button\" onclick=\"submitForm()\">Submit</button>\n</p>\n</form>");
-    
+    s.push("<form id=\"myForm\">")
+    s.push("<b class=\"topText\">View Games by Team:</b>")
+    s.push("<p>")
+    s.push("<label for=\"teamId\">Team ID:</label>")
+    s.push("<input type=\"text\" id=\"teamId\" name=\"teamId\"><br><br>")
+    s.push("</p>")
+    s.push("<p>")
+    s.push("<button class=\"submitButton\" type=\"button\" onclick=\"submitForm()\">Submit</button>")
+    s.push("</p>")
+    s.push("</form>")
+
     var content = document.getElementById('inputs');
     content.innerHTML = s.join('')
 }
 
 function viewGamesByDateScreen() {
     var s = [];
-    s.push("<form id=\"myForm\">\n<b class=\"topText\">Add Game:</b>\n<p>\n<label for=\"teamId1\">Team 1 ID:</label>\n<input type=\"text\" id=\"teamId1\" name=\"teamId1\"><br><br>");
-    s.push("\n</p>\n<p>\n<label for=\"teamId2\">Team 2 ID:</label>\n<input type=\"text\" id=\"teamId2\" name=\"teamId2\"><br><br>\n</p>\n<p>\n<label for=\"score1\">Team 1 Score:</label>");
-    s.push("\n<input type=\"text\" id=\"score1\" name=\"score1\"><br><br>\n</p>\n<p>\n<label for=\"score1\">Team 1 Score:</label>\n<input type=\"text\" id=\"score1\" name=\"score1\"><br><br>");
-    s.push("\n</p>\n<p>\n<label for=\"score2\">Team 2 Score:</label>\n<input type=\"text\" id=\"score2\" name=\"score2\"><br><br>\n</p>\n<p>\n<label for=\"date\">Date:</label>\n<input type=\"text\" id=\"date\" name=\"date\"><br><br>");
-    s.push("\n</p>\n<p>\n<button class=\"submitButton\" type=\"button\" onclick=\"submitForm()\">Submit</button>\n</p>\n</form>");
-    
+    s.push("<form id=\"myForm\">")
+    s.push("<b class=\"topText\">View Results by Date:</b>")
+    s.push("<p>")
+    s.push("<label for=\"date\">Date:</label>")
+    s.push("<input type=\"text\" id=\"date\" name=\"date\"><br><br>")
+    s.push("</p>")
+    s.push("<p>")
+    s.push("<button class=\"submitButton\" type=\"button\" onclick=\"submitForm()\">Submit</button>")
+    s.push("</p>")
+    s.push("</form>")
+
     var content = document.getElementById('inputs');
     content.innerHTML = s.join('')
 }
